6be430742cd76b4ca18d43daf0b5c8b1c09aa16d
YatFungLoo <looyatfung@gmail.com>
2025-01-23 19:51:33 +0800

    Using stacks to arrange views
    https://developer.apple.com/tutorials/app-dev-training/using-stacks-to-arrange-views

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by YatFungLoo on 23/1/2025.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value:3, total:9)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elasped").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("10 mintues")
+        Circle().strokeBorder(lineWidth: 40)
+        HStack {
+            Text("Speaker 1 of 3")
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: "forward.fill")
+            }
+            .accessibilityLabel("Next speaker")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
