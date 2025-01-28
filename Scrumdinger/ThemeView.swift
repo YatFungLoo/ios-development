70b0edf0fac9e2753e790010dc64b6f9f1cfecf5
YatFungLoo <looyatfung@gmail.com>
2025-01-28 12:49:37 +0800

    add ThemeView

---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,27 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by YatFungLoo on 28/1/2025.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .frame(maxWidth: .infinity)
+            .padding(4)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Preview: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .bubblegum)
+    }
+}
